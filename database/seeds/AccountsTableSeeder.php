bef9bcf86fff473684a7ff6b9d9623e6ea564ad5
Hairul Anam <hairulanam17@yahoo.co.id>
2018-06-12 10:06:49 +0700

    add akun admin

--- database/seeds/AccountsTableSeeder.php
+++ database/seeds/AccountsTableSeeder.php
@@ -18,42 +18,55 @@ class AccountsTableSeeder extends Seeder
             'level'     => '0',
             'avatar_path'   => '/avatars/default.jpg',
         ]);
-    	DB::table('users')->where('id', '>', '1')->delete();
-    	DB::table('authorities')->where('id', '>', '1')->delete();
-    	$faker = Faker\Factory::create('id_ID');
-    	$data2 = [];
-    	foreach(DB::table('employees')->inRandomOrder()->take(15)->get() as $d){
-    		$level = $faker->randomElement(['1', '2', '3']);
-    		if(App\User::where('level', '1')->count() > 7){
-    			$level = $faker->randomElement(['2', '3']);
-    		}
-    		if(App\User::where('level', '2')->count() > 5){
-    			$level = '3';
-    		}
-    		$username = $faker->username;
-    		$data = [
-    			'created_at' => $faker->dateTimeBetween('-1 years', '-7 days'),
-    			'level' => $level,
-    			'avatar_path' => '/avatars/default.jpg',
-    			'employee' => $d->id,
-    			'username' => $username,
-    			'password' => bcrypt($username),
-    		];
-    		$user = App\User::create($data);
-    		$data2[] = [
-    			"departments" => $faker->randomElement(["1", "0"]),
-    			"positions" => $faker->randomElement(["1", "0"]),
-    			"employees" => $faker->randomElement(["1", "0"]),
-    			"salary_rules" => $faker->randomElement(["1", "0"]),
-    			"attendances" => $faker->randomElement(["1", "0"]),
-    			"over_time" => $faker->randomElement(["1", "0"]),
-    			"loans" => $faker->randomElement(["1", "0"]),
-    			"accounts" => $faker->randomElement(["1", "0"]),
-    			"salaries" => $faker->randomElement(["1", "0"]),
-    			"company_profile" => $faker->randomElement(["1", "0"]),
-    			"user" => $user->id
-    		];
-    	}
-		DB::table('authorities')->insert($data2);
-    }
+        DB::table('authorities')->insert([
+            "departments"           => '1',
+            "positions"             => '1',
+            "employees"             => '1',
+            "salary_rules"          => '1',
+            "attendances"           => '1',
+            "over_time"             => '1',
+            "loans"                 => '1',
+            "accounts"              => '1',
+            "salaries"              => '1',
+            "company_profile"       => '1',
+            "user"                  => '1',
+        ]);
+        DB::table('users')->where('id', '>', '1')->delete();
+        DB::table('authorities')->where('id', '>', '1')->delete();
+        $faker = Faker\Factory::create('id_ID');
+        $data2 = [];
+        foreach(DB::table('employees')->inRandomOrder()->take(15)->get() as $d){
+          $level = $faker->randomElement(['1', '2', '3']);
+          if(App\User::where('level', '1')->count() > 7){
+             $level = $faker->randomElement(['2', '3']);
+         }
+         if(App\User::where('level', '2')->count() > 5){
+             $level = '3';
+         }
+         $username = $faker->username;
+         $data = [
+             'created_at' => $faker->dateTimeBetween('-1 years', '-7 days'),
+             'level' => $level,
+             'avatar_path' => '/avatars/default.jpg',
+             'employee' => $d->id,
+             'username' => $username,
+             'password' => bcrypt($username),
+         ];
+         $user = App\User::create($data);
+         $data2[] = [
+             "departments" => $faker->randomElement(["1", "0"]),
+             "positions" => $faker->randomElement(["1", "0"]),
+             "employees" => $faker->randomElement(["1", "0"]),
+             "salary_rules" => $faker->randomElement(["1", "0"]),
+             "attendances" => $faker->randomElement(["1", "0"]),
+             "over_time" => $faker->randomElement(["1", "0"]),
+             "loans" => $faker->randomElement(["1", "0"]),
+             "accounts" => $faker->randomElement(["1", "0"]),
+             "salaries" => $faker->randomElement(["1", "0"]),
+             "company_profile" => $faker->randomElement(["1", "0"]),
+             "user" => $user->id
+         ];
+     }
+     DB::table('authorities')->insert($data2);
+ }
 }
